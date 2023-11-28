215601c9c763b872f33c2bed6c8c4bacf5890261
Mahesh Singh Madai <maheshmadai977@gmail.com>
2023-11-28 17:45:44 +0545

    expense category

--- financepersonal.client/src/app/app.module.ts
+++ financepersonal.client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
